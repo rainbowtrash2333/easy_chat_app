1e7431db40b249f7f1d126291a4a2b15afd7935f
liushiyi2333 <50539450+liushiyi2333@users.noreply.github.com>
2019-12-11 16:23:53 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -27,3 +27,49 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+library appengine.api.errors;
+
+import 'dart:io';
+
+class AppEngineError implements Exception {
+  final String message;
+
+  const AppEngineError(this.message);
+
+  @override
+  String toString() => 'AppEngineException: $message';
+}
+
+class NetworkError extends AppEngineError implements IOException {
+  NetworkError(String message) : super(message);
+
+  @override
+  String toString() => 'NetworkError: $message';
+}
+
+class ProtocolError extends AppEngineError implements IOException {
+  static const ProtocolError INVALID_RESPONSE =
+      ProtocolError('Invalid response');
+
+  const ProtocolError(String message) : super(message);
+
+  @override
+  String toString() => 'ProtocolError: $message';
+}
+
+class ServiceError extends AppEngineError {
+  final String serviceName;
+
+  ServiceError(String message, {this.serviceName = 'ServiceError'})
+      : super(message);
+
+  @override
+  String toString() => '$serviceName: $message';
+}
+
+class ApplicationError extends AppEngineError {
+  ApplicationError(String message) : super(message);
+
+  @override
+  String toString() => 'ApplicationError: $message';
+}
