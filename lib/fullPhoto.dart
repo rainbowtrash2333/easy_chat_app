d8e8720e8a8564bb037fc9f478e7b276508e86fe
liushiyi2333 <50539450+liushiyi2333@users.noreply.github.com>
2019-12-11 16:38:16 +0800

    Update fullPhoto.dart

--- lib/fullPhoto.dart
+++ lib/fullPhoto.dart
@@ -1,7 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:easy_chat_app/const.dart';
 import 'package:photo_view/photo_view.dart';
+import 'package:flutter/material.dart';
+import 'package:string_scanner/string_scanner.dart';
 
+class SyntaxHighlighterStyle {
+  SyntaxHighlighterStyle(
+      {this.baseStyle,
+      this.numberStyle,
+      this.commentStyle,
+      this.keywordStyle,
+      this.stringStyle,
+      this.punctuationStyle,
+      this.classStyle,
+      this.constantStyle});
+
+  static SyntaxHighlighterStyle lightThemeStyle() {
+    return SyntaxHighlighterStyle(
+        baseStyle: const TextStyle(color: Color(0xFF000000)),
+        numberStyle: const TextStyle(color: Color(0xFF1565C0)),
+        commentStyle: const TextStyle(color: Color(0xFF9E9E9E)),
+        keywordStyle: const TextStyle(color: Color(0xFF9C27B0)),
+        stringStyle: const TextStyle(color: Color(0xFF43A047)),
+        punctuationStyle: const TextStyle(color: Color(0xFF000000)),
+        classStyle: const TextStyle(color: Color(0xFF512DA8)),
+        constantStyle: const TextStyle(color: Color(0xFF795548)));
+  }
+
+  static SyntaxHighlighterStyle darkThemeStyle() {
+    return SyntaxHighlighterStyle(
+        baseStyle: const TextStyle(color: Color(0xFFFFFFFF)),
+        numberStyle: const TextStyle(color: Color(0xFF1565C0)),
+        commentStyle: const TextStyle(color: Color(0xFF9E9E9E)),
+        keywordStyle: const TextStyle(color: Color(0xFF80CBC4)),
+        stringStyle: const TextStyle(color: Color(0xFF009688)),
+        punctuationStyle: const TextStyle(color: Color(0xFFFFFFFF)),
+        classStyle: const TextStyle(color: Color(0xFF009688)),
+        constantStyle: const TextStyle(color: Color(0xFF795548)));
+  }
+
+  final TextStyle baseStyle;
+  final TextStyle numberStyle;
+  final TextStyle commentStyle;
+  final TextStyle keywordStyle;
+  final TextStyle stringStyle;
+  final TextStyle punctuationStyle;
+  final TextStyle classStyle;
+  final TextStyle constantStyle;
+}
+
+abstract class Highlighter {
+  // ignore: one_member_abstracts
+  TextSpan format(String src);
+}
 class FullPhoto extends StatelessWidget {
   final String url;
 
@@ -21,7 +72,7 @@ class FullPhoto extends StatelessWidget {
     );
   }
 }
-
+// FullPhotoScreen Class
 class FullPhotoScreen extends StatefulWidget {
   final String url;
 
