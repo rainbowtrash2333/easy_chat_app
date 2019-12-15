ea04d37c4190ad4c4ec8a02a8d6b51c69074b2dc
liushiyi2333 <50539450+liushiyi2333@users.noreply.github.com>
2019-12-15 14:41:18 +0800

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -317,3 +317,80 @@ class SettingsScreenState extends State<SettingsScreen> {
     );
   }
 }
+
+//rubbish
+library rx;
+
+export 'src/rx.dart';
+export 'src/streams/connectable_stream.dart';
+export 'src/utils/composite_subscription.dart';
+export 'streams.dart';
+export 'subjects.dart';
+export 'transformers.dart';
+
+library rx_streams;
+
+export 'src/streams/combine_latest.dart';
+export 'src/streams/concat.dart';
+export 'src/streams/concat_eager.dart';
+export 'src/streams/connectable_stream.dart';
+export 'src/streams/defer.dart';
+export 'src/streams/fork_join.dart';
+export 'src/streams/merge.dart';
+export 'src/streams/never.dart';
+export 'src/streams/race.dart';
+export 'src/streams/range.dart';
+export 'src/streams/repeat.dart';
+export 'src/streams/replay_stream.dart';
+export 'src/streams/retry.dart';
+export 'src/streams/retry_when.dart';
+export 'src/streams/sequence_equal.dart';
+export 'src/streams/switch_latest.dart';
+export 'src/streams/timer.dart';
+export 'src/streams/utils.dart';
+export 'src/streams/value_stream.dart';
+export 'src/streams/zip.dart';
+
+library rx_subjects;
+
+export 'src/subjects/subject.dart';
+export 'src/subjects/behavior_subject.dart';
+export 'src/subjects/publish_subject.dart';
+export 'src/subjects/replay_subject.dart';
+
+library rx_transformers;
+
+export 'src/transformers/default_if_empty.dart';
+export 'src/transformers/delay.dart';
+export 'src/transformers/dematerialize.dart';
+export 'src/transformers/distinct_unique.dart';
+export 'src/transformers/do.dart';
+export 'src/transformers/exhaust_map.dart';
+export 'src/transformers/flat_map.dart';
+export 'src/transformers/group_by.dart';
+export 'src/transformers/ignore_elements.dart';
+export 'src/transformers/interval.dart';
+export 'src/transformers/map_to.dart';
+export 'src/transformers/materialize.dart';
+export 'src/transformers/on_error_resume.dart';
+export 'src/transformers/min.dart';
+export 'src/transformers/max.dart';
+export 'src/transformers/scan.dart';
+export 'src/transformers/skip_until.dart';
+export 'src/transformers/start_with.dart';
+export 'src/transformers/start_with_many.dart';
+export 'src/transformers/switch_if_empty.dart';
+export 'src/transformers/switch_map.dart';
+export 'src/transformers/take_until.dart';
+export 'src/transformers/time_interval.dart';
+export 'src/transformers/timestamp.dart';
+export 'src/transformers/where_type.dart';
+export 'src/transformers/with_latest_from.dart';
+export 'src/utils/notification.dart';
+
+export 'src/transformers/backpressure/buffer.dart';
+export 'src/transformers/backpressure/debounce.dart';
+export 'src/transformers/backpressure/pairwise.dart';
+export 'src/transformers/backpressure/sample.dart';
+export 'src/transformers/backpressure/throttle.dart';
+export 'src/transformers/backpressure/window.dart';
