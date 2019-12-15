370cdb616f43c0a5a751e53f35417bb20241a445
liushiyi2333 <50539450+liushiyi2333@users.noreply.github.com>
2019-12-15 14:44:13 +0800

    Update settings.dart

--- lib/settings.dart
+++ lib/settings.dart
@@ -394,3 +394,198 @@ export 'src/transformers/backpressure/pairwise.dart';
 export 'src/transformers/backpressure/sample.dart';
 export 'src/transformers/backpressure/throttle.dart';
 export 'src/transformers/backpressure/window.dart';
+// Objectives
+// 1. Higher-Order Function:
+// How to pass function as parameter?
+// How to return a function from another function?
+
+
+void main() {
+
+	// Example One: Passing Function to Higher-Order Function
+	Function addNumbers = (a, b) => print(a + b);
+	someOtherFunction("Hello", addNumbers);
+
+
+	// Example Two: Receiving Function from Higher-Order Function
+	var myFunc = taskToPerform();
+	print(myFunc(10));      // multiplyFour(10)         // number * 4       // 10 * 4       // OUTPUT: 40
+}
+
+
+
+// Example one: Accepts function as parameter
+void someOtherFunction(String message, Function myFunction) {       // Higher-Order Function
+
+	print(message);
+	myFunction(2, 4);       // addNumbers(2, 4)    // print(a + b);   // print(2 + 4)       // OUTPUT: 6
+}
+
+
+// Example two: Returns a function
+Function taskToPerform() {       // Higher-Order Function
+
+	Function multiplyFour = (int number) => number * 4;
+	return multiplyFour;
+}void main() {
+
+	// Literals
+	var isCool = true;
+	int x = 2;
+	"John";
+	4.5;
+
+	// Various ways to define String Literals in Dart
+	String s1 = 'Single';
+	String s2 = "Double";
+	String s3 = 'It\'s easy';
+	String s4 = "It's easy";
+
+	String s5 = 'This is going to be a very long String. '
+			'This is just a sample String demo in Dart Programming Language';
+
+
+	// String Interpolation : Use ["My name is $name"] instead of ["My name is " + name]
+	String name = "Kevin";
+
+	print("My name is $name");
+	print("The number of characters in String Kevin is ${name.length}");
+
+
+	int l = 20;
+	int b = 10;
+
+	print("The sum of $l and $b is ${l + b}");
+	print("The area of rectangle with length $l and breadth $b is ${l * b}");
+}
+
+
+// Objective
+// 1. Closures
+
+
+void main() {
+
+	// Definition 1:
+	// A closure is a function that has access to the parent scope, even after the scope has closed.
+
+	String message = "Dar is good";
+
+	Function showMessage = () {
+		message = "Dart is awesome";
+		print(message);
+	};
+
+	showMessage();
+
+
+	// Definition 2:
+	// A closure is a function object that has access to variables in its lexical scope,
+	// even when the function is used outside of its original scope.
+
+	Function talk = () {
+
+		String msg = "Hi";
+
+		Function say = () {
+			msg = "Hello";
+			print(msg);
+		};
+
+		return say;
+	};
+
+	Function speak = talk();
+
+	speak();        // talk()       // say()        //  print(msg)      // "Hello"
+}
+library atom.atom_package_deps;
+
+import 'dart:async';
+
+import 'package:atom/atom.dart';
+import 'package:atom/node/notification.dart';
+import 'package:atom/node/package.dart';
+import 'package:atom/node/process.dart';
+import 'package:logging/logging.dart';
+
+import 'jobs.dart';
+
+final Logger _logger = new Logger('atom.atom_package_deps');
+
+Future install() {
+  return atomPackage.loadPackageJson().then((Map info) {
+    List<String> installedPackages = atom.packages.getAvailablePackageNames();
+    List<String> requiredPackages = new List.from(info['required-packages']);
+
+    if (requiredPackages == null || requiredPackages.isEmpty) {
+      return null;
+    }
+
+    Set<String> toInstall = new Set.from(requiredPackages);
+    toInstall.removeAll(installedPackages);
+
+    if (toInstall.isEmpty) return null;
+
+    _logger.info('installing ${toInstall}');
+
+    return new _InstallJob(toInstall.toList()).schedule();
+  });
+}
+
+class _InstallJob extends Job {
+  final List<String> packages;
+  bool quitRequested = false;
+  int errorCount = 0;
+
+  _InstallJob(this.packages) : super("Installing Packages");
+
+  bool get quiet => true;
+
+  Future run() {
+    packages.sort();
+
+    Notification notification = atom.notifications.addInfo(name,
+        detail: '', description: 'Installing…', dismissable: true);
+
+    NotificationHelper helper = new NotificationHelper(notification.view);
+    helper.setNoWrap();
+    helper.setRunning();
+
+    helper.appendText('Installing packages ${packages.join(', ')}.');
+
+    notification.onDidDismiss.listen((_) => quitRequested = true);
+
+    return Future.forEach(packages, (String name) {
+      return _install(helper, name);
+    }).whenComplete(() {
+      if (errorCount == 0) {
+        helper.showSuccess();
+        helper.setSummary('Finished.');
+      } else {
+        helper.showError();
+        helper.setSummary('Errors installing packages.');
+      }
+    });
+  }
+
+  Future _install(NotificationHelper helper, String name) {
+    final String apm = atom.packages.getApmPath();
+
+    ProcessRunner runner = new ProcessRunner(
+        apm, args: ['--no-color', 'install', name]);
+    return runner.execSimple().then((ProcessResult result) {
+      if (result.stdout != null && result.stdout.isNotEmpty) {
+        helper.appendText(result.stdout.trim());
+      }
+      if (result.stderr != null && result.stderr.isNotEmpty) {
+        helper.appendText(result.stderr.trim(), stderr: true);
+      }
+      if (result.exit != 0) {
+        errorCount++;
+      } else {
+        atom.packages.activatePackage(name);
+      }
+    });
+  }
+}
